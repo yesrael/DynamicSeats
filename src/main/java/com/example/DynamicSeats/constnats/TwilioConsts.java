a9b69f8a534514dec54b619cf14b071741ae3c46
yesra <yesrael@gmail.com>
2023-03-18 14:59:27 +0200

    Adding Emojis from: https://emojipedia.org/ and fixing the bug of duplicate message response

--- src/main/java/com/example/DynamicSeats/constnats/TwilioConsts.java
+++ src/main/java/com/example/DynamicSeats/constnats/TwilioConsts.java
@@ -2,11 +2,13 @@ package com.example.DynamicSeats.constnats;
 
 import com.twilio.type.PhoneNumber;
 
+import java.time.ZonedDateTime;
+
 public class TwilioConsts {
 
     // Find your Account Sid and Token at twilio.com/console
     public static final String ACCOUNT_SID = "ACcf2055b6f5b49de783b61a43b28e7f31";
-    public static final String AUTH_TOKEN = "0d86635431378188e22da61b375f1da3";
+    public static final String AUTH_TOKEN = "888fd9bd236d2685918750aeb07f48b9";
     // The WhatsApp number to read messages from
     public static final PhoneNumber WHATSAPP_NUMBER = new PhoneNumber("whatsapp:+14155238886");
     public static final String WHATSAPP_USER_NAME = "Israel";
@@ -14,6 +16,5 @@ public class TwilioConsts {
     public static final long DELAY_MS = 1000;
     public static final String TO_PHONE_NUMBER = "whatsapp:+972543150898";
     public static final String FROM_PHONE_NUMBER = "whatsapp:+14155238886";
-
 
 }
--- src/main/java/com/example/DynamicSeats/service/ChooseSeatBot.java
+++ src/main/java/com/example/DynamicSeats/service/ChooseSeatBot.java
@@ -21,18 +21,19 @@ public class ChooseSeatBot implements CommandLineRunner {
 
     @Override
     public void run(String... args) throws Exception {
-        writeMessageService.writeMessage("Hello " + TwilioConsts.WHATSAPP_USER_NAME + "\nFrom Department הפצת מידע ללקוחות" +
+        writeMessageService.writeMessage("Hello " + TwilioConsts.WHATSAPP_USER_NAME + " \uD83E\uDDD1\u200D\uD83D\uDCBC" + "\nFrom Department הפצת מידע ללקוחות" +
                 ",\nWelcome to the seats choose bot \n" +
-                "Please Choose on which Floor do you want to seat today?" +
-                "\n1 \n2 \n3");
+                "Please Choose on which Floor do you want to seat today? \uD83D\uDCBA" +
+                "\n1️⃣ \n2️⃣ \n3️⃣");
 
         Message lastMessage = null;
+        ZonedDateTime currentZonedDateTime = ZonedDateTime.now();
 
         // Continuously fetch and print new messages
         while (true) {
             ResourceSet<Message> messages = Message.reader()
                     .setTo(TwilioConsts.WHATSAPP_NUMBER)
-                    .setDateSentAfter(ZonedDateTime.now())
+                    .setDateSentAfter(currentZonedDateTime)
                     .read();
 
             // Get the latest message if there are any new messages
@@ -43,37 +44,37 @@ public class ChooseSeatBot implements CommandLineRunner {
             if (!messageList.isEmpty()) {
                 Message latestMessage = messageList.get(0);
 
-                // If the latest message is different than the last message,
+                // If the latest message is different from the last message,
                 // print the message and send Response
-                if (!latestMessage.equals(lastMessage)) {
+                if (lastMessage==null || !latestMessage.getBody().equals(lastMessage.getBody())) {
                     log.info(latestMessage.getBody());
                     switch (latestMessage.getBody()) {
                         case "1":
                         case "2":
                         case "3":
-                            writeMessageService.writeMessage("Please choose Favorite area in Floor" + latestMessage.getBody()
-                                    + ": \nA\nB\nC\nD");
+                            writeMessageService.writeMessage("Please choose Favorite area in Floor-" + latestMessage.getBody().trim()
+                                    + ": \n▫️A\n▫️B\n▫️C\n▫️D");
                             break;
                         case "A":
                         case "B":
                         case "C":
                         case "D":
                             writeMessageService.writeMessage("The available seats there are: \nSeat a1\nSeat b3\nSeat c7" +
-                                    "\nPlease type the seat you want including the word \"Seat\" at the beginning");
+                                    "\n\n"+ "‼️Please type the seat you want including the word \"Seat\" at the    beginning");
                             break;
                         default:
                             if (latestMessage.getBody().contains("Seat")) {
                                 writeMessageService.writeMessage(latestMessage.getBody() + " was Occupied Successfully," +
-                                        "\nIt will be yours until the end of the day, \nHave a nice day.");
+                                        "\nIt will be yours until the end of the day, \nHave a nice day. \uD83C\uDF3B");
                             } else {
                                 writeMessageService.writeMessage("Not Available option, Please try again");
                             }
                             break;
                     }
                     lastMessage = latestMessage;
+                    currentZonedDateTime = ZonedDateTime.now();
                 }
             }
-
             // Wait before checking for new messages again
             try {
                 Thread.sleep(TwilioConsts.DELAY_MS);
